6c14cf9b9cca8890d7a9cb5aee10d6cbebfbc177
Ayas Mahmud <ayasmahmud48@gmail.com>
2025-08-23 16:07:29 +0600

    added some functionality

---
+++ main.js
@@ -0,0 +1,69 @@
+// ADD MONEY BTN
+let addMoneyBtn = document.getElementById('add-money-btn');
+
+// Global Pin
+const pin = 1234;
+
+addMoneyBtn.addEventListener("click",(e)=>{
+    let amountAdd = parseInt(document.getElementById('amountToAdd').value);
+    let nitAmount = parseInt(document.getElementById('nit-amount').innerText);
+    let acountNumber = document.getElementById('acountNumber').value;
+    let pinNumber = parseInt(document.getElementById('pinNumber').value);
+    e.preventDefault()
+    
+    if(pinNumber!==pin){
+        alert("Incorrect Pin")
+        return
+    }
+    if(acountNumber.length == 11){
+        let total = nitAmount + amountAdd
+        document.getElementById('nit-amount').innerText = total
+        alert("Amount add successfully")
+    }else{
+        alert("Please provide 11 digit account number")
+    }
+})
+
+// CASH OUT BTN
+let withdrawBtn = document.getElementById('withdrawBtn');
+withdrawBtn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    console.log("hello world")
+    let agentNumber = document.getElementById('CashOut-A-N').value;
+    let cashoutpin = parseInt(document.getElementById('cashOut-pin').value);
+    let cashAmount = parseInt(document.getElementById('cashAmount').value);
+    let nitAmount = parseInt(document.getElementById('nit-amount').innerText);
+    console.log(nitAmount)
+    console.log(cashAmount)
+    console.log(agentNumber.length)
+    console.log(cashoutpin)
+    if(cashoutpin!==pin){
+        alert("Incorrect Pin")
+        return
+    }
+    if(agentNumber.length == 11){
+        let total = nitAmount - cashAmount
+        document.getElementById('nit-amount').innerText = total
+        alert("Cash Out successfully")
+    }else{
+        alert("Please provide 11 digit account number")
+    }
+})
+
+
+
+// click
+let clickaddMoney = document.getElementById('clickaddMoney');
+let clickCaseOut = document.getElementById('clickCaseOut');
+// click section
+let addMoneySection = document.getElementById('addMoneySection');
+let cashoutSection = document.getElementById('cashoutSection');
+clickaddMoney.addEventListener('click',()=>{
+    addMoneySection.setAttribute('class','')
+    cashoutSection.setAttribute('class','hidden')
+})
+
+clickCaseOut.addEventListener('click',()=>{
+    cashoutSection.setAttribute('class','')
+    addMoneySection.setAttribute('class','hidden')
+})
--- script.js
+++ script.js
@@ -4,12 +4,14 @@ document.getElementById("loginbtn").addEventListener("click",(e)=>{
     e.preventDefault()
     const mobilenumber = 1863199573;
     const pinNumber = 1234;
-    console.log(number.value)
-    console.log(pin.value)
+
     if(mobilenumber==number.value && pinNumber==pin.value){
         window.location.href = "main.html";
     }else{
-        if(mobilenumber!=number.value){
+        if(number.value=='' && pin.value==''){
+            alert("Please Enter Mobile Number and Pin")
+        }
+        else if(mobilenumber!=number.value){
             alert("Invalid Mobile Number")
         }else{
             alert("Invalid Pin")
